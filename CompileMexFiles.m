9a08c910c334e0abb942372d1bf758eec5a0acbe
alinakrause <76391336+alinakrause@users.noreply.github.com>
2025-01-03 21:10:05 +0100

    Update CompileMexFiles.m

--- CompileMexFiles.m
+++ CompileMexFiles.m
@@ -1,11 +1,6 @@
 
 % Use mex to compile all mex files
-mex CalcRheoBaseEIF.c
-mex EIF2DSpatialNetworkNoJitter.c
-mex EIF2DSpatialNetworkGainMod.c 
-mex EIFTwoPopNetwork.c
 mex EIF2DSpatialNetworkNonlinearities.c
-mex IF2DSpatialNetwork.c
-mex IF2DSpatialNetworkNonlinearities.c
-mex IF2D_NL_test.c
-mex IF2DSpatialNetworkDendrites.c
+mex LIF2DSpatialNetwork.c
+mex LIF2DSpatialNetworkNonlinearities.c
+mex LIF2DSpatialNetworkDendrites.c
